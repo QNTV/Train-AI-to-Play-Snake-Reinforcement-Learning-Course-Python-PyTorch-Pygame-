dfd98b07a90b961633db8afde79a11789517d9b7
Quang Nguyen <quangn8888@gmail.com>
2022-06-23 11:44:44 -0700

    snake

--- snake.py
+++ snake.py
@@ -1,41 +1,57 @@
-# import curses
-#
-#
-# # setup window
-# curses.initscr()
-# win = curses.newwin(20, 60, 0, 0) # 20 rows, 60 columns, 0 row, 0 column
-# win.keypad(1)
-# curses.noecho()
-# curses.curs_set(0)
-# win.border(0)
-# win.nodelay(1) # -1 for blocking, 0 for non-blocking
-#
-# # snake and food
-# snake = [(4,10), (4,9), (4,8)]
-# food = (10,20)
-#
-#
-#
-# # game logic
-# score = 0
-#
-# ESC = 27
-# key = curses.KEY_RIGHT
-#
-#
-# while True:
-#     event = win.getch()
-#
-#     for c in snake:
-#         win.addch(c[0], c[1], '*')
-#
-#     win.addch(food[0], food[1], '#')
-#
-#
-#
-# curses.endwin()
-# print(f"Final score = {score}")
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+pygame.init()
 
-#https://www.youtube.com/watch?v=M_npdRYD4K0
-#09:00
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
 
+Point = namedtuple('Point', 'x', 'y')
+
+BLOCK_SIZE = 20
+class SnakeGame:
+    def __int__(self, w=640, h=480):
+        self.w=w
+        self.h=h
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+
+        #init game state
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, Point(self.head.x-BLOCK_SIZE, self.head.y),
+                                 Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        self.score = 0
+        self.food = None
+        self._place_food()
+
+    def _place_food(self):
+        x = random.randint(0, (self.w-BLOCK_SIZE)//BLOCK_SIZE)*BLOCK_SIZE
+        y = random.randint(0, (self.h-BLOCK_SIZE)//BLOCK_SIZE)*BLOCK_SIZE
+
+    def play_step(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = SnakeGame()
+
+    # game loop
+    while True:
+        game.play_step()
+
+        # break if game is over
+
+
+    pygame.quit()
+
+
+#https://www.youtube.com/watch?v=--nsd2ZeYvs
+#03:52:00
